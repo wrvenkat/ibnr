f2724a8dc136812fd4c98d7c68a2b9ed4f4034ad
W.R.Venkat <wrvenkat@gmail.com>
2017-01-01 15:39:18 -0500

    Properly removing test files from master branch.

--- test/simpletest.sh
+++
@@ -1,9 +0,0 @@
-
-penkernelver=''
-while read first kernelver rest
-do
-    printf "Uninstalling kernel version: %s ....\n" "$kernelver"
-    penkernelver=$(printf "%s %s\n" "$penkernelver" "$kernelver")
-done < <(dpkg --list | grep -e ii[[:space:]]*linux-image.*)
-
-printf "KernelVersions are %s" "$penkernelver"
